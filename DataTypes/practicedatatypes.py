26bd7aea4c1b16dbeb74c126ec90064f294bfc21
Aditya-Lamaniya <lamaniya.aditya@gmail.com>
2021-06-19 17:28:50 +0530

    first commit

---
+++ DataTypes/practicedatatypes.py
@@ -0,0 +1,12 @@
+a=10
+b=66.0
+c=-7
+print(a,b,c)
+x=0.2
+y=22/7
+print(x,y)
+print(type(y))
+m=0b1010              #for binary ,its 0b
+n=0xaf                #for hexa decimal
+o=0o77                #for octa 
+print(m,n,o)
